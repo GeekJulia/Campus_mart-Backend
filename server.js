fee6b65a4446c00007f0e867752aa3978ac240ef
GeekJulia <juliayaks148@gmail.com>
2025-04-03 15:13:36 +0100

    Cleaning up A

--- server.js
+++ server.js
@@ -1,4 +1,51 @@
 const express = require('express')
 const app = express()
+const bcrypt = require("bcrypt")
+
+app.use(express.json())
+
+const users = [] // this list of users will be in a database, this is for testing purposes
+app.get("/users", (req , res) => {
+    res.json(users)
+})
+
+app.post("/users", async (req,res) => { //create a user,hashing the password and saving it in users list or database
+    try{
+        const salt = await bcrypt.genSalt() //we put await in front cause it is asynchronus
+        const hashedPassword = await bcrypt.hash(req.body.password,salt)
+        console.log(salt)
+        console.log(hashedPassword)
+        const user = {
+            name: req.body.name,
+            password: hashedPassword
+        }
+        users.push(user)
+        res.status(201).send()
+    }
+    catch{
+        res.status(500).send()
+    }
+})
+
+app.post("/users/login", async (req,res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if(user == null){
+        return res.status(400).send("Cannot find user")
+    }
+    try{
+        if(await bcrypt.compare(req.body.password,user.password)){
+            res.send("Success")
+        }
+        else{
+            res.send("Incorrect Password.Please try again")
+        }
+    }
+    catch{
+        res.status(500).send()
+    }
+})
+
+app.delete
+
 
 app.listen(3000, () => {console.log("Server is listening on port 3000")})

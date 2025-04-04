a5183d97bbc899f5678e7f2c052d4edc691bd8a4
GeekJulia <juliayaks148@gmail.com>
2025-04-05 00:14:55 +0100

    Added some comments and features

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express")
 const app = express()
 
-
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-
-const AuthRouter = require("./routes/Auth_user")
-app.use(AuthRouter)
-
+//Routes start from here
+const authRouter = require("./routes/Auth_user")
+app.use(authRouter)
 
 app.listen(3000, () => {console.log("Server is listening on port 3000")})
